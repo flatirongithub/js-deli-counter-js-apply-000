a3537cbb54a525d4afa567d83705b1a32d0dbf3e
N <nirav2000+flatiron@gmail.com>
2018-05-16 14:58:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var katzDeliLine = [];
-
+var counter;
 function takeANumber (katzDeliLine, name) {
+  counter++;
   katzDeliLine.push(name);
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
 
+
+console.log(takeANumber(katzDeliLine, counter))
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length>0) {
-    var nowserving = katzDeliLine[0];
-    katzDeliLine.splice(0,1);
+    var nowserving =   katzDeliLine.splice(0,1);
     return "Currently serving " + nowserving + ".";
   } else {
     return "There is nobody waiting to be served!";
@@ -19,7 +21,7 @@ function currentLine (katzDeliLine) {
   if (katzDeliLine.length>0) {
     var lineList=[];
     for (var line in katzDeliLine) {
-      lineList.push(" "+(Number(line)+1)+". "+katzDeliLine[line]);
+      lineList.push(" "+((line)+1)+". "+katzDeliLine[line]);
     }
     return "The line is currently:" + lineList;
   } else {
